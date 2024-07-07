bda74d05d00cc2f231673bd6993463949587d35a
ricksonmenezes <ricksonmenezes@gmail.com>
2024-07-07 16:49:12 +0800

    creating quiz links

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,13 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-<section><h1>nextjs + typescript</h1></section>
+<section>
+  <h1>All Quizzes</h1>
+  <ul>
+    <li><a href="/quiz/1"> Quiz 1</a></li>
+    <li><a href="/quiz/2"> Quiz 2</a></li>
+    <li><a href="/quiz/3"> Quiz 3</a></li>
+  </ul>
+</section>
   );
 }
