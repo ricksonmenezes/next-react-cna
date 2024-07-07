bab2676152dd58ae9daec536c40e071ad11b67cf
ricksonmenezes <ricksonmenezes@gmail.com>
2024-07-07 17:02:33 +0800

    added Link component for faster transition without full page load

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,17 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
 <section>
   <h1>All Quizzes</h1>
   <ul>
-    <li><a href="/quiz/1"> Quiz 1</a></li>
-    <li><a href="/quiz/2"> Quiz 2</a></li>
-    <li><a href="/quiz/3"> Quiz 3</a></li>
+    <li><Link href="/quiz/1">
+      Quiz 1</Link></li>
+    <li><Link href="/quiz/2">
+      Quiz 2</Link></li>
+    <li><Link href="/quiz/3">
+      Quiz 3</Link></li>
   </ul>
 </section>
   );
