ac4904ca72a86fe21efade111f7a7af3e00217b7
ricksonmenezes <ricksonmenezes@gmail.com>
2024-07-10 19:11:03 +0800

    inline blue

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,21 @@
 import Image from "next/image";
 import Link from "next/link";
 import postgres  from "postgres";
+import {Suspense} from "react";
 
 const sql = postgres(process.env.DATABASE_URL)
-
 type Quiz = {
   quiz_id:number,
   title: string
 }
 async function Quizzes() {
 
+
   const quizzes: Quiz[] = await sql `select * from "Quiz"."Quizzes"`;
   return(
       <ul>
         {quizzes.map((quiz) => (
-            <li key={quiz.id}>
+            <li key={quiz.quiz_id}>
               <Link href={`/quiz/${quiz.quiz_id}`}>{quiz.title}</Link>
             </li>
             ))}
@@ -24,9 +25,11 @@ async function Quizzes() {
 export default function Home() {
   return (
 <section>
-  <h1>All Quizzes</h1>
-  <Quizzes />
 
+  <h1 className="text-2xl font-semibold text-blue-700">All Quizzes</h1>
+  <Suspense fallback={<p>Loading.......</p>}>
+    <Quizzes />
+  </Suspense>
 </section>
   );
 }
